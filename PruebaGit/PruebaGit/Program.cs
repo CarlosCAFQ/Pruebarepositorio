0444c3c46ac93197360af221f1a17fde1b2821be
CarlosCAFQ <Administrador@HUASCARAN>
2017-04-12 13:54:52 -0500

    Segundo cambio

--- PruebaGit/PruebaGit/Program.cs
+++ PruebaGit/PruebaGit/Program.cs
@@ -18,6 +18,7 @@ namespace PruebaGit
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             // Comentario de prueba
+            // Segundo comentario
         }
     }
 }
